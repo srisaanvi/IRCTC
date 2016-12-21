f298578a5524299776ca22b814810df921813dcd
dec <sri.asunder@gmail.com>
2016-12-21 13:25:13 +0530

    added a project

---
+++ retail/src/main/java/com/in/retail/App.java
@@ -0,0 +1,13 @@
+package com.in.retail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
